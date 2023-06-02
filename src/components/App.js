97217d2df55158d6f37641733bd8ceb2c35b2784
hyehae <sdahye908@gmail.com>
2023-06-02 22:10:01 +0900

    Add authentication function

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useEffect, useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, [])
   return (
-    <AppRouter />
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/>  : "Initializing..."}
+      <footer>&copy; TwitterClone {new Date().getFullYear()}</footer>
+    </>
   );
 }
 
